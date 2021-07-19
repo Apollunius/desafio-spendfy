d608afc1d6c1b3c2dc1a515b6e55f5927da7dad1
Thauan Costa <thauan.costa@residencia.dev>
2021-07-19 01:24:19 -0300

    fix: eslint fix

--- src/controllers/Document.ts
+++ src/controllers/Document.ts
@@ -10,10 +10,7 @@ export async function createDocument(req: Request, res: Response) {
   try {
     validateDocument({ name, content });
 
-    const checkName = await knex("documents")
-      .first("*")
-      .where("name", name)
-      .andWhere("deletedAt", null);
+    const checkName = await knex("documents").first("*").where("name", name).andWhere("deletedAt", null);
 
     if (!checkName) {
       const buff = Buffer.from(content);
@@ -21,81 +18,75 @@ export async function createDocument(req: Request, res: Response) {
       const kbSize = buff.byteLength / 1000; // transformando o de byte para kbyte
 
       const document = await knex("documents")
-        .insert({ kbSize, name, content: base64data })
-        .returning(["id", "name", "content", "kbSize"]);
+	  .insert({ kbSize, name, content: base64data })
+	  .returning(["id", "name", "content", "kbSize"]);
 
       return res.status(201).json(document);
     }
     return res.status(400).json({ error: "Name already exists!" });
   } catch (err) {
-    return res
-      .status(err.status || 500)
-      .json({ error: err.message || "Algo deu errado" });
+
+    return res.status(err.status || 500).json({ error: err.message || "Algo deu errado" });
   }
 }
 
 export async function getDocuments(_: Request, res: Response) {
   try {
     const documents = await knex("documents")
-      .select("id", "name", "content", "kbSize")
-      .where("deletedAt", null)
-      .orderBy("createdAt", "desc");
+	.select("id", "name", "content", "kbSize")
+	.where("deletedAt", null)
+	.orderBy("createdAt", "desc");
 
     return res.status(201).json(documents);
   } catch (err) {
-    return res
-      .status(err.status || 500)
-      .json({ error: err.message || "Algo deu errado" });
+
+    return res.status(err.status || 500).json({ error: err.message || "Algo deu errado" });
   }
 }
 
 export async function getDocument(req: Request, res: Response) {
   const { documentId } = req.params;
+
   try {
     validateId(documentId);
 
-    const id = await knex("documents")
-      .first("*")
-      .where("id", documentId)
-      .andWhere("deletedAt", null);
+    const id = await knex("documents").first("*").where("id", documentId).andWhere("deletedAt", null);
+
     if (id) {
       const document = await knex("documents")
-        .select("id", "name", "content", "kbSize")
-        .where("id", documentId)
+	  	.select("id", "name", "content", "kbSize")
+	  	.where("id", documentId)
         .andWhere("deletedAt", null)
         .orderBy("createdAt", "desc");
 
       return res.status(201).json(document);
     }
+
     return res.status(404).json({ error: "Document not found!" });
   } catch (err) {
-    return res
-      .status(err.status || 500)
-      .json({ error: err.message || "Algo deu errado" });
+
+    return res.status(err.status || 500).json({ error: err.message || "Algo deu errado" });
   }
 }
 
 export async function deleteDocument(req: Request, res: Response) {
   const { documentId } = req.params;
+
   try {
     validateId(documentId);
 
-    const id = await knex("documents")
-      .first("*")
-      .where("id", documentId)
-      .andWhere("deletedAt", null);
+    const id = await knex("documents").first("*").where("id", documentId).andWhere("deletedAt", null);
+
     if (id) {
-      await knex("documents")
-        .update("deletedAt", new Date())
-        .where("id", documentId);
+
+      await knex("documents").update("deletedAt", new Date()).where("id", documentId);
 
       return res.status(200).json({ message: "Deleted document!" });
     }
 
     return res.status(404).json({ error: "Document not found!" });
   } catch (err) {
-    return res
-      .status(err.status || 500)
-      .json({ error: err.message || "Algo deu errado" });
+
+    return res.status(err.status || 500).json({ error: err.message || "Algo deu errado" });
   }
 }
