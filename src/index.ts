4385beb851bbc414068718c69825195605ceb03c
Thauan Costa <thauan.costa@residencia.dev>
2021-07-19 00:21:09 -0300

    feat: adding knex configurations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import routes from "./routes";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 
+// knex.migrate.latest();
 const port = process.env.PORT ?? 9999;
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
