1a9932c5450b242db9172f81cae01d7957dee2c7
Thauan Costa <thauan.costa@residencia.dev>
2021-07-19 01:09:32 -0300

    feat: get document/documents

--- src/controllers/Document.ts
+++ src/controllers/Document.ts
@@ -1,9 +1,12 @@
 import type { Request, Response } from "express";
-import knex from "../database/index";
+
+import knex from "../database";
 import validateDocument from "../helpers/validators/DocumentValidator";
+import validateId from "../helpers/validators/IdValidator";
 
 export async function createDocument(req: Request, res: Response) {
   const { name, content } = req.body;
+
   try {
     validateDocument({ name, content });
 
@@ -15,7 +18,7 @@ export async function createDocument(req: Request, res: Response) {
     if (!checkName) {
       const buff = Buffer.from(content);
       const base64data = buff.toString("base64");
-      const kbSize = buff.byteLength / 1000; //transformando o de byte para kbyte
+      const kbSize = buff.byteLength / 1000; // transformando o de byte para kbyte
 
       const document = await knex("documents")
         .insert({ kbSize, name, content: base64data })
@@ -23,9 +26,51 @@ export async function createDocument(req: Request, res: Response) {
 
       return res.status(201).json(document);
     }
-
     return res.status(400).json({ error: "Name already exists!" });
   } catch (err) {
-    return res.status(err.status || 500).json({ error: err.message || "Algo deu errado" });
+    return res
+      .status(err.status || 500)
+      .json({ error: err.message || "Algo deu errado" });
+  }
+}
+
+export async function getDocuments(_: Request, res: Response) {
+  try {
+    const documents = await knex("documents")
+      .select("id", "name", "content", "kbSize")
+      .where("deletedAt", null)
+      .orderBy("createdAt", "desc");
+
+    return res.status(201).json(documents);
+  } catch (err) {
+    return res
+      .status(err.status || 500)
+      .json({ error: err.message || "Algo deu errado" });
+  }
+}
+
+export async function getDocument(req: Request, res: Response) {
+  const { documentId } = req.params;
+  try {
+    validateId(documentId);
+
+    const id = await knex("documents")
+      .first("*")
+      .where("id", documentId)
+      .andWhere("deletedAt", null);
+    if (id) {
+      const document = await knex("documents")
+        .select("id", "name", "content", "kbSize")
+        .where("id", documentId)
+        .andWhere("deletedAt", null)
+        .orderBy("createdAt", "desc");
+
+      return res.status(201).json(document);
+    }
+	return res.status(404).json({error: "Document not found!"})
+  } catch (err) {
+    return res
+      .status(err.status || 500)
+      .json({ error: err.message || "Algo deu errado" });
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
-import { Router} from 'express';
-import { createDocument } from './controllers/Document';
+import { Router } from "express";
+
+import { createDocument, getDocument, getDocuments } from "./controllers/Document";
 
 const routes = Router();
 
 routes.post("/documents", createDocument);
-routes.get("/documents");
-routes.get("documents/:documentId");
-routes.delete("/documents/:documentId")
+routes.get("/documents", getDocuments);
+routes.get("/documents/:documentId", getDocument);
+routes.delete("/documents/:documentId");
 
 export default routes;
