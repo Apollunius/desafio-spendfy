4e15fb861d459dfaa422cc42308c07f3a691842c
Thauan Costa <thauan.costa@residencia.dev>
2021-07-19 01:18:31 -0300

    fix: remove unique constraint

--- src/migrations/20210718175912_documents.ts
+++ src/migrations/20210718175912_documents.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("documents", table => {
 	  table.increments("id")
 	  table.text("kbSize");
-	  table.text("name").unique().notNullable();
+	  table.text("name").notNullable();
 	  table.text("content").notNullable();
 	  table.timestamp("createdAt").defaultTo(knex.fn.now())
 	  table.timestamp("deletedAt")
