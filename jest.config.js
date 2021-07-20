dba813b4dbdb7cc255a8f31799585181a617e40e
Thauan Costa <thauan.costa@residencia.dev>
2021-07-19 23:16:53 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/knexfile.ts",
+    "/node_modules/",
+    "/src/helpers",
+    "/src/migrations/",
+    "/tests/",
+  ],
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+      isolatedModules: true,
+    },
+  },
+  modulePaths: ["<rootDir>/src/"],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts?(x)"],
+};
